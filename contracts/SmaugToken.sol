1d5b9c17d9bc22871b8ed10142b416833b5afe74
StoyanD <s.dimitrov19@gmail.com>
2017-11-26 16:15:45 -0800

    adding constructor tests

--- contracts/SmaugToken.sol
+++ contracts/SmaugToken.sol
@@ -10,10 +10,8 @@ contract SmaugToken is StandardToken, Ownable{
   uint8 public constant decimals = 18;
   uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
-  modifier validAddresses(address[] addressArr){
-    for(uint i = 0; i < addressArr; i++){
-      require(addressArr[i] != address(0));
-    }
+  modifier validAddress(address _address){
+    require(_address != address(0));
     _;
   }
 
@@ -25,13 +23,12 @@ contract SmaugToken is StandardToken, Ownable{
     balances[msg.sender] = INITIAL_SUPPLY;
   }
 
-  function adminTransfer(address from, address to, uint256 _value) public onlyOwner validAddress([from,to]) returns (bool) {
+  function adminTransfer(address _from, address _to, uint256 _value) public onlyOwner validAddress(_from) validAddress(_to) returns (bool) {
     // SafeMath.sub will throw if there is not enough balance.
-    balances[from] = balances[from].sub(_value);
-    balances[to] = balances[to].add(_value);
-    Transfer(from, to, _value);
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(_from, _to, _value);
     return true;
   }
-
 
 }
--- test/smaug_token_test.js
+++ test/smaug_token_test.js
@@ -7,4 +7,20 @@ contract('SmaugToken', function(accounts){
   beforeEach(async function() {
     instance = await SmaugToken.new();
   });
+
+  it("should initialize the token contract with the right owner and initial balance", async function(){
+    let val = await instance.decimals.call();
+    assert.equal(val.valueOf(), 18, "decimals should be 18");
+    let owner = await instance.owner();
+    assert.equal(owner.valueOf(), accounts[0], "the owner should be the first account");
+
+    let tokens = await instance.totalSupply();
+    let ownerTokens = await instance.balanceOf(owner);
+    assert.equal(ownerTokens.valueOf(), tokens.valueOf(), "The owner should have all the tokens at contract init");
+    assert.equal(tokens.valueOf(), 1e+22, "initial token supply should be 10,000 * 1e+18( ie the decimal precision we want)");
+  });
+
+  it("should only allow owner to call forcibly transfer token balances", async function(){
+
+  });
 });//end contract tests
